4d41ac690596712eb2b3d6cf2ba27d01cbf72fcc
Pablo Alonso <pablo.alonso@upf.edu>
2025-02-05 10:32:30 +0100

    Gather keymaps to the same file
    Instead of having them scattered across plugin config files
    Note that some lsp-related keymaps are still kept there.

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -25,10 +25,8 @@ vim.api.nvim_set_keymap("n", "K", "5k", {noremap=false})
 vim.api.nvim_set_keymap("v", "J", "5j", {noremap=false})
 vim.api.nvim_set_keymap("v", "K", "5k", {noremap=false})
 vim.api.nvim_set_keymap("n", "<leader>w", ":update<cr>", {noremap=false, silent=true})
--- Copilot
-vim.g['copilot_no_tab_map'] = true
-vim.g['copilot_assume_mapped'] = true
-vim.api.nvim_set_keymap('i', '<C-a>', 'copilot#Accept("<CR>")', {expr=true, silent=true})
+
+
 -- keymap to yank to system's clipboard in normal and visual modes
 vim.api.nvim_set_keymap('v', '<leader>y', '"+y', {noremap=true})
 vim.api.nvim_set_keymap('n', '<leader>y', '"+y', {noremap = true})
@@ -36,6 +34,13 @@ vim.api.nvim_set_keymap('n', '<leader>y', '"+y', {noremap = true})
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 -- vim.keymap.set({ 'n', 'v' }, '<space>', '<Nop>', {silent=true})
+
+-- Keymaps for plugins
+
+-- Copilot
+vim.g['copilot_no_tab_map'] = true
+vim.g['copilot_assume_mapped'] = true
+vim.api.nvim_set_keymap('i', '<C-a>', 'copilot#Accept("<CR>")', {expr=true, silent=true})
 
 -- Keymaps for Leap
 vim.keymap.set('n',        '<leader>f', '<Plug>(leap)')
@@ -51,3 +56,53 @@ vim.keymap.set('n', '<leader>om', ':ObsidianTomorrow<CR>', {silent=true})
 vim.keymap.set('n', '<leader>od', ':ObsidianDailies<CR>', {silent=true})
 vim.keymap.set('n', '<leader>on', ':ObsidianNew<CR>', {silent=true})
 vim.keymap.set('n', '<leader>so', ':ObsidianSearch<CR>', {silent=true})
+
+-- keymaps for substitute
+vim.keymap.set("n", "s", require('substitute').operator, { noremap = true })
+vim.keymap.set("n", "ss", require('substitute').line, { noremap = true })
+vim.keymap.set("n", "S", require('substitute').eol, { noremap = true })
+vim.keymap.set("x", "s", require('substitute').visual, { noremap = true })
+-- keymaps for substitute over a range
+vim.keymap.set("n", "<space>r", require('substitute.range').operator, { noremap = true })
+vim.keymap.set("x", "<space>r", require('substitute.range').visual, { noremap = true })
+vim.keymap.set("n", "<space>rr", require('substitute.range').word, { noremap = true })
+
+-- See `:help telescope.builtin`
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer]' })
+
+vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>sS', require('telescope.builtin').git_status, { desc = '' })
+vim.keymap.set("n", "<leader>sr", "<CMD>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>", silent)
+vim.keymap.set("n", "<leader>sR", "<CMD>lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>", silent)
+vim.keymap.set("n", "<leader>sn", "<CMD>lua require('telescope').extensions.notify.notify()<CR>", silent)
+
+-- Function to search on the dotfiles directory defined by the environment variable DOTFILES_PATH
+vim.keymap.set("n", "<leader>s.", function()
+  local cwd = vim.env.DOTFILES_PATH
+  if not cwd or cwd == "" then
+    vim.api.nvim_err_writeln("Environment variable 'DOTFILES_PATH' is not set or is empty")
+    return
+  end
+  require("telescope.builtin").find_files({ cwd = cwd })
+end, { silent = true })
+
+-- vim.keymap.set("n", "<Leader>sb", ":Telescope git_branches <CR>", silent)
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
--- nvim/lua/plugins/tele.lua
+++ nvim/lua/plugins/tele.lua
@@ -27,38 +27,5 @@ require('telescope').setup {
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
 
--- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer]' })
-
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>sS', require('telescope.builtin').git_status, { desc = '' })
-vim.keymap.set("n", "<leader>sr", "<CMD>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>", silent)
-vim.keymap.set("n", "<leader>sR", "<CMD>lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>", silent)
-vim.keymap.set("n", "<leader>sn", "<CMD>lua require('telescope').extensions.notify.notify()<CR>", silent)
-
--- Function to search on the dotfiles directory defined by the environment variable DOTFILES_PATH
-vim.keymap.set("n", "<leader>s.", function()
-  local cwd = vim.env.DOTFILES_PATH
-  if not cwd or cwd == "" then
-    vim.api.nvim_err_writeln("Environment variable 'DOTFILES_PATH' is not set or is empty")
-    return
-  end
-  require("telescope.builtin").find_files({ cwd = cwd })
-end, { silent = true })
-
--- vim.keymap.set("n", "<Leader>sb", ":Telescope git_branches <CR>", silent)
-
 
 -- vim: ts=2 sw=2 et
