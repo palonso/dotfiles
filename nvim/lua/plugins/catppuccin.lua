1659e400fc3d4399c3d6799b2abe6d546de2b492
Pablo Alonso <pablo.alonso@upf.edu>
2025-02-24 19:03:52 +0100

    Set transparent background

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,6 +1,12 @@
 return {
   -- Configure LazyVim to load gruvbox
   {
+    "catppuccin",
+    opts = {
+      transparent_background = true,
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
