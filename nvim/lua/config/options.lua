a4d32340ef8d1f4b1cd3983ee22932951aee391b
Pablo Alonso <pablo.alonso@upf.edu>
2025-04-02 15:38:44 +0200

    Support copying to system buffer from remote servers

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -7,3 +7,35 @@ vim.opt.mouse = ""
 
 -- Do not provide ai suggestions as completion entries
 vim.g.ai_cmp = false
+
+-- Support copying to system's buffer from remote servers (OSC 52) also inside tmux sessions
+-- https://github.com/neovim/neovim/discussions/29350#discussioncomment-10299517
+vim.opt.clipboard = "unnamedplus"
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
+if vim.env.TMUX ~= nil then
+  local copy = { "tmux", "load-buffer", "-w", "-" }
+  local paste = { "bash", "-c", "tmux refresh-client -l && sleep 0.05 && tmux save-buffer -" }
+  vim.g.clipboard = {
+    name = "tmux",
+    copy = {
+      ["+"] = copy,
+      ["*"] = copy,
+    },
+    paste = {
+      ["+"] = paste,
+      ["*"] = paste,
+    },
+    cache_enabled = 0,
+  }
+end
