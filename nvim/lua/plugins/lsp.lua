5806829a9da8c572da587d4bf807c913c65b1967
Pablo Alonso <pablo.alonso@upf.edu>
2025-04-02 15:39:30 +0200

    Do not show buffer source for completions until 4 chars are pressed

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -55,7 +55,7 @@ return {
           { name = "nvim_lsp" },
           { name = "path" },
         }, {
-          { name = "buffer" },
+          { name = "buffer", keyword_length = 4 },
         }),
         formatting = {
           format = function(entry, item)
