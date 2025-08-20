5a5a95142e70ef2c226f852778fb38fb4dfe2e5b
Pablo Alonso <pablo.alonso@upf.edu>
2025-08-20 14:44:51 +0200

    Attemp to integrate blink_cmp
    However not working so far

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -4,6 +4,13 @@ return {
     -- NOTE: This is a temporary fix until lazyvim colorescheme updates get() -> get_*()
     commit = "9a9a875",
     opts = {
+      integrations = {
+        -- NOTE: This is not working for new and I couldn't find the reason so far
+        blink_cmp = {
+          enabled = true,
+          style = "bordered",
+        },
+      },
       transparent_background = true,
       float = {
         transparent = true,
