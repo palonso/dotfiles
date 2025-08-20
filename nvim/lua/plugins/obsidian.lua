6397d9f19681e8b692c39ee3c62e80e944ce6512
Pablo Alonso <pablo.alonso@upf.edu>
2025-08-20 14:39:51 +0200

    Add template for obsidian notes

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -23,9 +23,11 @@ return {
         path = vim.fn.getenv("OBSIDIAN_WORKSPACE_PATH"),
       },
     },
+
     -- Optional, if you keep notes in a specific subdirectory of your vault.
     notes_subdir = "0_inbox",
     new_notes_location = "notes_subdir",
+
     -- Daily notes configuration.
     daily_notes = {
       -- Optional, if you keep daily notes in a separate directory.
@@ -37,8 +39,9 @@ return {
       -- Optional, default tags to add to each new daily note created.
       default_tags = { "daily-notes" },
       -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-      template = nil,
+      template = "daily.md",
     },
+
     -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
     completion = {
       -- Set to false to disable completion.
@@ -46,5 +49,25 @@ return {
       -- Trigger completion at 2 chars.
       min_chars = 2,
     },
+
+    -- Optional, for templates (see below).
+    templates = {
+      folder = "templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+      -- A map for custom variables, the key should be the variable and the value a function
+      substitutions = {},
+    },
+
+    -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
+    -- URL it will be ignored but you can customize this behavior here.
+    ---@param url string
+    follow_url_func = function(url)
+      -- Open the URL in the default web browser.
+      vim.fn.jobstart({ "open", url }) -- Mac OS
+      -- vim.fn.jobstart({"xdg-open", url})  -- linux
+      -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
+      -- vim.ui.open(url) -- need Neovim 0.10.0+
+    end,
   },
 }
