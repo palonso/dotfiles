21dc873189177e608304ec0240b37504354e530e
Pablo Alonso <pablo.alonso@upf.edu>
2025-09-18 19:45:38 +0200

    Update to lazyvim 15
    - Prevent fixing plugin version
    - update versions

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -2,7 +2,7 @@ return {
   {
     "catppuccin",
     -- NOTE: This is a temporary fix until lazyvim colorescheme updates get() -> get_*()
-    commit = "9a9a875",
+    -- commit = "9a9a875",
     opts = {
       integrations = {
         -- NOTE: This is not working for new and I couldn't find the reason so far
