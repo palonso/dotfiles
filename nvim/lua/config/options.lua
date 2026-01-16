e774d1ecab1ee9c18df7b24e7f8d35ae1eb2d761
Pablo Alonso <pablo.alonso@upf.edu>
2026-01-16 10:38:11 +0100

    Activate mouse support

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,8 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Deactivate mouse support
-vim.opt.mouse = ""
+-- Aactivate mouse support
+-- palonso: I mainly want mouse support for scrolling the terminal, (e.g., for the cluade chat window)
+vim.opt.mouse = "a"
 
 -- Do not provide ai suggestions as completion entries
 vim.g.ai_cmp = false
